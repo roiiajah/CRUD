600b6b959d330211b05d524fc045e27a077dbe28
Rsann <139483578+roiiajah@users.noreply.github.com>
2025-07-22 03:05:30 +0700

    initial commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,5 +10,4 @@ $routes->get('/mahasiswa/create', 'Mahasiswa::create');
 $routes->resource('mahasiswa', ['controller' => 'Mahasiswa']);
 $routes->post('/mahasiswa/store', 'Mahasiswa::store');
 
-$routes->post('mahasiswa/update/(:num)', 'Mahasiswa::update/$1');
 $routes->post('mahasiswa/delete/(:num)', 'Mahasiswa::delete/$1');
